6150b3bd66cb977685e536d42688aa6380e4ddbf
BigOoga <edoardo.ruffi@gmail.com>
2021-10-01 15:41:00 +0200

    Added footer, bottomfooter

---
+++ laravel-comics/resources/views/includes/bottomfooter.blade.php
@@ -0,0 +1,27 @@
+<div id="bottom-footer">
+    <div class="d-flex justify-content-around align-items-center h-100">
+      <div class="btn-signup text-uppercase">Sign up now!</div>
+      <div class="links">
+        <ul>
+          <li>
+            <a class="fs-2 text-blue text-uppercase" href="">Follow us</a>
+          </li>
+          <li>
+            <a href=""><img src="{{ asset('img/footer-facebook.png') }}" alt=""/></a>
+          </li>
+          <li>
+            <a href=""><img src="{{ asset('img/footer-twitter.png') }}" alt=""/></a>
+          </li>
+          <li>
+            <a href=""><img src="{{ asset('img/footer-youtube.png') }}" alt=""/></a>
+          </li>
+          <li>
+            <a href=""><img src="{{ asset('img/footer-pinterest.png') }}" alt=""/></a>
+          </li>
+          <li>
+            <a href=""><img src="{{ asset('img/footer-periscope.png') }}" alt=""/></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
---
+++ laravel-comics/resources/views/includes/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div id="logo">
+      <img src="{{ asset('img/dc-logo.png') }}" alt="Logo" />
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a>
+            Link statico
+         </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
